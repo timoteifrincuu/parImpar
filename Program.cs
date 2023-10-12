a7af0d47f2652b2d79ae784958626e3b54dd86e4
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2023-10-12 21:56:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace parImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
